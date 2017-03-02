43574a96b7c4c752c27b0632453126fca41526c5
Albina Khafizova <al-binki@yandex.ru>
2017-03-02 23:11:57 +0300

    Задание 8

---
+++ Задание 8/UnitTestProject2/UnitTest1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+using System.Collections.Generic;
+
+namespace UnitTestProject2
+{
+    [TestFixture]
+    public class homework8
+    {
+    private IWebDriver driver;
+    private WebDriverWait wait;
+
+    [SetUp]
+    public void start()
+    {
+        driver = new ChromeDriver();
+        wait = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
+    }
+
+    [Test]
+    public void VerifyProductStickers()
+    {
+        driver.Url = "http://localhost/litecart";
+        IList<IWebElement> products = driver.FindElements(By.CssSelector("li.product.column.shadow.hover-light"));
+        foreach (IWebElement i in products)
+        {
+            Assert.IsTrue(i.FindElements(By.CssSelector(".sticker")).Count == 1);
+        }
+    }
+
+    [TearDown]
+    public void stop()
+    {
+        driver.Quit();
+        driver = null;
+    }
+  }
+}
